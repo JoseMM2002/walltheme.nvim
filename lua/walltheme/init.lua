6ea13fff83af82d667cd9b268b127664023f58ce
JoseMM2002 <jeolmmt@gmail.com>
2024-12-17 01:39:53 -0600

    add config

--- lua/walltheme/init.lua
+++ lua/walltheme/init.lua
@@ -1,10 +1,27 @@
 local M = {}
 
-local function load_theme()
-	package.path = package.path .. ";~/.cache/walltheme/?.lua"
+local function load_colors()
+	local home = os.getenv("HOME") -- Obtiene /home/usuario
+	local walltheme_path = home .. "/.cache/walltheme/?.lua"
 
+	package.path = package.path .. ";" .. walltheme_path -- Cargar el módulo
 	local colors = require("colors")
-	vim.notify(vim.inspect(colors))
+
+	return colors
+end
+
+local function highlight_all(colors)
+	local highlights = require("config")()
+
+	for group, properties in pairs(highlights) do
+		vim.api.nvim_set_hl(0, group, properties)
+	end
+end
+
+local function load_theme()
+	local colors = load_colors()
+	vim.opt.termguicolors = true
+	highlight_all(colors)
 end
 
 -- Función para cargar el archivo de colores
