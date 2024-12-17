3edfe2d9e2d42f270fa7b643583393a839050cbf
JoseMM2002 <jeolmmt@gmail.com>
2024-12-17 01:11:01 -0600

    test

--- lua/walltheme/init.lua
+++ lua/walltheme/init.lua
@@ -1,29 +1,14 @@
 local M = {}
 
 -- Ruta al archivo de colores en .cache
-local colors_path = vim.fn.expand("~/.cache/walltheme/colors.lua")
+local colors_path = "~/.cache/walltheme/colors.lua"
 
 local function load_theme()
-	local ok, colorscheme = pcall(require, "walltheme")
-	if not ok then
-		print("Colorscheme not found at " .. colors_path)
-		return
-	end
+	package.path = package.path .. ";~/.cache/wallthemes/?.lua"
 
-	vim.notify(colorscheme, vim.log.levels.INFO)
-	local colors = colorscheme.colors
+	local colors = require("colors")
 
-	-- Aplicar colores
-	vim.opt.termguicolors = true
-	vim.api.nvim_set_hl(0, "Normal", { fg = colors.foreground, bg = colors.background })
-	vim.api.nvim_set_hl(0, "Cursor", { fg = colors.cursor, bg = colors.cursor })
-	vim.api.nvim_set_hl(0, "LineNr", { fg = colors.color8, bg = colors.background })
-	vim.api.nvim_set_hl(0, "Comment", { fg = colors.color8, italic = true })
-	vim.api.nvim_set_hl(0, "Keyword", { fg = colors.color1, bold = true })
-	vim.api.nvim_set_hl(0, "String", { fg = colors.color2 })
-	vim.api.nvim_set_hl(0, "Function", { fg = colors.color4, bold = true })
-
-	vim.notify("Colorscheme loaded", vim.log.levels.INFO)
+	print("El color de fondo es: " .. colors.background)
 end
 
 -- Función para cargar el archivo de colores
